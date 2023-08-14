9826b98645b714bb15345c76a2c81e38c5c3af20
Rajesh-Reel <rajreel2@hotmail.com>
2023-08-14 12:16:31 +0100

    added row weights 7 kyu solution

---
+++ RowWeights.js
@@ -0,0 +1,33 @@
+/*
+
+7 kyu 
+
+Row Weights
+
+Scenario
+Several people are standing in a row divided into two teams.
+The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+Task
+Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+Notes
+Array size is at least 1.
+All numbers will be positive.
+Input >> Output Examples
+rowWeights([13, 27, 49])  ==>  return (62, 27)
+
+*/
+
+function rowWeights(array){
+    let teamOne = 0;
+    let teamTwo = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (i % 2 === 0) {
+        teamOne += array[i];
+      } else {
+        teamTwo += array[i];
+      }
+    }
+    return [teamOne, teamTwo];
+}
