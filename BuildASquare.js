de7df8bdeb6dbad1b80db4bcca591f0c16223a1b
Rajesh-Reel <rajreel2@hotmail.com>
2023-10-20 11:29:55 +0100

    added build a square 7 kyu solution

---
+++ BuildASquare.js
@@ -0,0 +1,21 @@
+/*
+
+7 kyu 
+
+Build a square 
+
+I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+
+*/
+
+function generateShape(integer) {
+    let plus = "+".repeat(integer);
+    return `${plus}\n`.repeat(integer).slice(0, -1);
+}
